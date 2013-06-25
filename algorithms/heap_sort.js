554ba78832afa1b7c29d8eddcb2d8370d7617e7d
Michael Sokol <mikaa123@gmail.com>
2013-06-25 10:33:40 -0700

    adding heapsort code

---
+++ algorithms/heap_sort.js
@@ -0,0 +1,152 @@
+function minHeapify(A, i) {
+
+// the minHeapify function re-arranges the nodes of a heap for `i`
+// being a misplaced node. the node is misplaced if the tree doesn't
+// satisfy the heap property.
+
+// in the worst case, it goes through all the depth of the tree,
+// giving it a O(log n) worst case.
+
+  var left  = (2*i) + 1,  // the index of the child on the left.
+      right = (2*i) + 2,  // the index of the child on the right.
+      smaller = i;        // the index of the smaller child.
+
+  if (left < A.length && A[left] < A[smaller]) {
+    smaller = left;
+  }
+
+  if (right < A.length && A[right] < A[smaller]) {
+    smaller = right;
+  }
+
+  if (smaller !== i) {
+    swap(A, i, smaller);
+    minHeapify(A, smaller);
+  }
+
+  return A;
+}
+
+function swap(A, a, b) {
+
+// let's define a convinient function that swaps indices `a` and `b`
+// in place in `A`.
+
+    var tmp = A[a];
+    A[a] = A[b];
+    A[b] = tmp;
+}
+
+function buildMinHeap(A) {
+
+// builds a min-heap from an array.
+// minHeapify each parent nodes from the bottom up.
+// the last parent in the array A can be found with
+// Math.floor(A.length/2).
+
+  if (!A) return;
+  if (!A.length) return A;
+
+  for (var i = Math.floor(A.length) - 1; i >= 0; i--) {
+    minHeapify(A, i);
+  }
+
+  return A;
+}
+
+function upHeap(A, i) {
+
+// percolates up the node at indice i.
+
+  var parent = Math.floor((i-1)/2);
+
+  if (i > 0 && A[i] < A[parent]) {
+    swap(A, i, parent);
+    upHeap(A, parent);
+  }
+}
+
+var MinHeap = function (A) {
+
+// MinHeap is the heap abstraction. it uses the functions
+// defined above to implement the heaps operations.
+
+  this.heap = buildMinHeap(A);
+};
+
+MinHeap.prototype.toArray = function () {
+
+// returns the array representation of the heap.
+
+  return this.heap;
+};
+
+MinHeap.prototype.remove = function () {
+
+// removes the first element of the heap.
+
+  var result = this.heap.shift();
+
+  if (this.heap.length) {
+    this.heap.unshift(this.heap.pop());
+    minHeapify(this.heap, 0);
+  }
+
+  return result;
+};
+
+MinHeap.prototype.insert = function (key) {
+
+// inserts the given key in the heap.
+
+  this.heap.push(key);
+  upHeap(this.heap, this.heap.length - 1);
+  return this;
+};
+
+function heapSort(A) {
+  var heap = new MinHeap(A),
+      key,
+      result = [];
+
+  if (!A) return;
+  if (!A.length) return A;
+
+  while (key = heap.remove()) {
+    result.push(key);
+  }
+
+  return result;
+}
+
+module.exports = {
+  'heapSort([]) => []': function (test) {
+    test.deepEqual(heapSort([]), []);
+    test.done();
+  },
+
+  'heapSort([1]) => [1]': function (test) {
+    test.deepEqual(heapSort([1]), [1]);
+    test.done();
+  },
+
+  'heapSort([1, 2, 3]) => [1, 2, 3]': function (test) {
+    test.deepEqual(heapSort([1, 2, 3]), [1, 2, 3]);
+    test.done();
+  },
+
+  'heapSort([3, 2, 1]) => [1, 2, 3]': function (test) {
+    test.deepEqual(heapSort([3, 2, 1]), [1, 2, 3]);
+    test.done();
+  },
+
+  'heapSort([1, 5, 2, 1]) => [1, 1, 2, 5]': function (test) {
+    test.deepEqual(heapSort([1, 5, 2, 1]), [1, 1, 2, 5]);
+    test.done();
+  },
+
+  'heapSort([9, 10, 25, 7, 2, 3, 4, 1, 8]) => [1, 2, 3, 4, 7, 8, 9, 10, 25]': function (test) {
+    test.deepEqual(heapSort([9, 10, 25, 7, 2, 3, 4, 1, 8]), [1, 2, 3, 4, 7, 8, 9, 10, 25]);
+    test.done();
+  }
+};
