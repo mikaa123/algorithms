f3b6853ff883172b70dc51913c0dede97f6e0b18
Michael Sokol <mikaa123@gmail.com>
2014-01-02 18:15:31 +0100

    bunch of functions for stats on bloom filter

--- algorithms/bloom_filter.scm
+++ algorithms/bloom_filter.scm
@@ -4,6 +4,16 @@
 
 (require-extension FNVHash)
 (require-extension PJWHash)
+(require-extension srfi-1)
+
+;;;;;;;;;;;;;;;;
+; Variables
+
+; The size of the bit vector.
+(define m 32)
+
+; The number of elements in the bloom filter.
+(define n 0);
 
 ; The bit-vector that's gonna be used as the filter.
 (define bloom-array (make-vector 32 0))
@@ -18,11 +28,20 @@
 	(lambda (str)
 		(modulo (PJWHash str) 32)));
 
+;;;;;;;;;;;;;;;;
+; Functions
+
+; Number of ones in the bloom filter
+(define X
+	(lambda ()
+		(reduce + 0 (vector->list bloom-array))))
+
 ; Hashes str and put it in the bloom array
-(define bloom-filter
+(define insert-element
 	(lambda (str)
 		(vector-set! bloom-array (hash1 str) 1)
-		(vector-set! bloom-array (hash2 str) 1)))
+		(vector-set! bloom-array (hash2 str) 1)
+		(set! n (add1 n))))
 
 ; Checks whether a string is contained in the bloom filter
 (define is-available
@@ -37,5 +56,14 @@
 ; .parameter k The number of hash functions.
 ; .parameter n the number of element in the set.
 (define false-positive-probability
-		(lambda (m k n)
-				(expt (- 1 (exp (/ (* (- k) n) m))) k)))
+		(lambda ()
+			(expt (- 1 (exp (/ (* (- 2) n) m))) 2)))
+
+(define optimal-hash-functions
+	(lambda ()
+			(* (/ m n) (log 2))))
+
+(define number-of-item
+	(lambda ()
+		(- (/ (* 32 (log (- 1 (/ (X) n)))) 2))))
+
